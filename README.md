1f83661737687a7c63bc4ef18164dc7824821fdf
NoiceNoiceBaby <lordthumper06@gmail.com>
2021-10-25 22:24:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### Dice Game
+The same as my dice game in python, but written in C
+---
+## requirements
+* `git clone https://github.com/NoiceNoiceBaby/dicegame_C.git`
+* `cd dicegame_C`
+* `mkdir assets/`
+* `nano highscores.txt` add 5 random usernames and scores in the format of "dave,23"
+* `nano logins.txt` - leave this empty, as user logins are created within the program
+* `cd..`
+---
+## building
+* `gcc game.c -o game -lm`
+* `./main`
+## disclaimer
+* THIS IS A WORKING PROJECT, AND IS NOT THE COMPLETED PROGRAM
---
+++ game.c
@@ -0,0 +1,376 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// INCLUSIONS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
+// system includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// system dependant includes
+#ifdef _WIN64
+#include <conio.h>
+#else
+#define clear_screen() printf("\e[1;1H\e[2J")
+#endif
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// DEFINITIONS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define TRUE 1
+#define FALSE 0
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ARRAY DECLARATION
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const int DICE[6] = {1, 2, 3, 4, 5, 6};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// MAIN MENU FUNCTIONS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int mainMenu(void)
+{
+    printf("########## 2 PLAYER DICE GAME ##########\n");
+    
+    int MAIN_MENU_LOOP = TRUE;
+    while (MAIN_MENU_LOOP == TRUE)
+    {
+        printf("1.) continue to login\n");
+        printf("2.) view high scores\n");
+        printf("3.) rules\n");
+        printf("4.) exit program");
+
+        int userInput;
+        scanf("%d", &userInput);
+    }
+
+    // return
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LOGIN FUNCTIONS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// temp buffer to hold usernames and passwords from logins.txt
+typedef struct userAndPassTypeTag
+{
+    char username[32];
+    char password[32];
+}
+userAndPassType;
+
+userAndPassType userData[16];
+
+int ADD_USER_TO_FILE (char* username, char* password)
+{
+    FILE *LOGINFILE = fopen("assets/logins.txt", "a");
+
+    if (LOGINFILE == NULL)
+    {
+        printf("assets/logins.txt is missing, please create this file\n");
+        return 1;
+    }
+
+    fprintf(LOGINFILE, "%s", username);
+    fprintf(LOGINFILE, ",");
+    fprintf(LOGINFILE, "%s", password);
+    fprintf(LOGINFILE, "\n");
+    fclose(LOGINFILE);
+
+    printf("%s has been validated", username);
+
+    // return
+    return 0;
+}
+
+int CHECK_USER (char* username, char *password)
+{
+    FILE *LOGINFILE = fopen("assets/logins.txt", "r");
+    
+    if (LOGINFILE == NULL)
+    {
+        printf("assets/logins.txt is missing, please create this file\n");
+        return 1;
+    }
+
+    char currentChar = 0;
+    char tempBuffer[32];
+    unsigned int structureIndex = 0u;
+    unsigned int bufferIndex = 0u;
+
+    while (currentChar != EOF)
+    {
+        currentChar = fgetc(LOGINFILE);
+
+        if (currentChar == ',') 
+        {
+            tempBuffer[bufferIndex] = 0;
+            strcpy(userData[structureIndex].username, tempBuffer);
+            bufferIndex = 0u;
+        }
+        else if (currentChar == '\n')
+        {
+            tempBuffer[bufferIndex] = 0;
+            strcpy(userData[structureIndex].password, tempBuffer);
+            bufferIndex = 0u;
+            structureIndex++;
+        }
+        else
+        {
+            tempBuffer[bufferIndex] = currentChar;
+            bufferIndex++;
+        }
+    }    
+    
+    fclose(LOGINFILE);
+    
+    if (strcmp(userData[0].username, username) == 0)
+    {
+        if (strcmp(userData[0].password, password) == 0)
+        {
+            printf("welcome %s", username);
+        }
+        else
+        {
+            printf("incorrect password\n");
+            USER_LOGIN();
+        }
+    }
+    else
+    {
+        printf("incorrect username\n");
+        USER_LOGIN();
+    }
+    
+    // return
+    return 0;
+}
+
+int CREATE_USER (void)
+{
+    int CREATE_USER_LOOP = TRUE;
+    while (CREATE_USER_LOOP == TRUE)
+    {
+        clear_screen();
+        
+        char username[15], password[15];
+        printf("enter your username for the new user\n");
+        scanf("%s", username);
+        printf("enter your password for the new user\n");
+        scanf("%s", password);
+
+        int confirmUsername;
+        printf("is %s your username? type '1' for yes, '2' for no\n", username);
+        scanf("%d", &confirmUsername);
+
+        if (confirmUsername == 1)
+        {
+            int confirmPassword;
+            printf("is %s your password? type '1' for yes, '2' for no\n", password);
+            scanf("%d", &confirmPassword);
+
+            if (confirmPassword == 1)
+            {
+                ADD_USER_TO_FILE(username, password);
+                CREATE_USER_LOOP = FALSE;
+            }
+            else
+            {
+                clear_screen();
+                continue;
+            }
+        }
+        else
+        {
+            clear_screen();
+            continue;
+        }
+
+    }
+    
+    // return
+    return 0;
+}
+
+int USER_LOGIN (void)
+{
+    int VALIDATION_LOOP = TRUE;
+    while (VALIDATION_LOOP == TRUE)
+    {
+        clear_screen();
+
+        char username[15], password[15];
+        printf("enter your username for the existing user\n");
+        scanf("%s", username);
+        printf("enter your password for the existing user\n");
+        scanf("%s", password);
+
+        int confirmUsername;
+        printf("is %s your username? type '1' for yes, '2' for no\n", username);
+        scanf("%d", &confirmUsername);
+
+        if (confirmUsername == 1)
+        {
+            int confirmPassword;
+            printf("is %s your password? type '1' for yes, '2' for no\n", password);
+            scanf("%d", &confirmPassword);
+
+            if (confirmPassword == 1)
+            {
+                CHECK_USER(username, password);
+                VALIDATION_LOOP = FALSE;
+            }
+            else
+            {
+                clear_screen();
+                continue;
+            }
+        }
+        else
+        {
+            clear_screen();
+            continue;
+        }
+    }
+    
+    // return 
+    return 0;
+}
+
+int player1Login (void)
+{
+    int PLAYER_1_LOGIN_LOOP = TRUE;
+    while (PLAYER_1_LOGIN_LOOP == TRUE)
+    {
+        clear_screen();
+        printf("##### PLAYER 1 LOGIN #####\n");
+        printf("1.) create new user\n");
+        printf("2.) use existing user\n");
+
+        int userInput;
+        scanf("%d", &userInput);
+
+        if (userInput == 1)
+        {
+            CREATE_USER();
+
+            printf("would you like to create another user? type '1' for yes, '2' for no\n");
+
+            int userInput;
+            scanf("%d", &userInput);
+
+            while (userInput == 1)
+            {
+                clear_screen();
+                CREATE_USER();
+
+                printf("would you like to create another user? type '1' for yes, '2' for no\n");
+
+                int userInput;
+                scanf("%d", &userInput);
+
+                if (userInput != 1)
+                {
+                    clear_screen();
+                    break;
+                }
+            }
+
+            USER_LOGIN();
+            break;
+        }
+        else if (userInput == 2)
+        {
+            USER_LOGIN();
+            break;
+        }
+        else
+        {
+            printf("invalid input - try again\n");
+            continue;
+        }
+    }
+    
+    // return
+    return 0;    
+}
+
+int player2Login (void)
+{
+    int PLAYER_2_LOGIN_LOOP = TRUE;
+    while (PLAYER_2_LOGIN_LOOP == TRUE)
+    {
+        clear_screen();
+        printf("##### PLAYER 2 LOGIN #####\n");
+        printf("1.) create new user\n");
+        printf("2.) use existing user\n");
+
+        int userInput;
+        scanf("%d", &userInput);
+
+        if (userInput == 1)
+        {
+            CREATE_USER();
+
+            printf("would you like to create another user? type '1' for yes, '2' for no\n");
+
+            int userInput;
+            scanf("%d", &userInput);
+
+            while (userInput == 1)
+            {
+                clear_screen();
+                CREATE_USER();
+
+                printf("would you like to create another user? type '1' for yes, '2' for no\n");
+
+                int userInput;
+                scanf("%d", &userInput);
+
+                if (userInput != 1)
+                {
+                    clear_screen();
+                    break;
+                }
+            }
+
+            USER_LOGIN();
+            break;
+        }
+        else if (userInput == 2)
+        {
+            USER_LOGIN();
+            break;
+        }
+        else
+        {
+            printf("invalid input - try again\n");
+            continue;
+        }
+    }
+    
+    // return
+    return 0;    
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// GAME FUNCTIONS
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// MAIN FUNCTION
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main (void)
+{
+    // main menu function call
+    //mainMenu();
+    // player login function calls
+    player1Login();
+    player2Login();
+    // return
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// END OF FILE
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
